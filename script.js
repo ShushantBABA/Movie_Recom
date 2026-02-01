ab08f180faf1ffacf703ef6f9c07b81be208e8ed
Shushant <shushant19102000@gmail.com>
2026-02-01 23:56:45 +0530

    Connected frontend to the railway backend

--- script.js
+++ script.js
@@ -80,7 +80,7 @@ async function sendMessage() {
 
     // 2. Send to Python Backend
     try {
-        let response = await fetch("http://127.0.0.1:5000/chat", {
+        let response = await fetch("movierecom-production.up.railway.app", {
             method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ message: message })
